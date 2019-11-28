0023a786e345930ff57a610687cc186793bdc025
Aleksey Kazarinov <aleksey.kaz@mail.ru>
2019-11-28 19:35:12 +0300

    S-3 change mustache to freemarker, filter page fixed

--- src/main/java/sweater/controller/MainController.java
+++ src/main/java/sweater/controller/MainController.java
@@ -26,9 +26,15 @@ public class MainController {
     }
 
     @GetMapping("/main")
-    public String main(Map<String, Object> model) {
+    public String main(@RequestParam(required = false, defaultValue = "") String filter, Model model) {
         Iterable<Message> messages = messageRepository.findAll();
-        model.put("messages", messages);
+        if (filter != null && !filter.isEmpty()) {
+            messages = messageRepository.findByTag(filter);
+        } else {
+            messages = messageRepository.findAll();
+        }
+        model.addAttribute("messages", messages);
+        model.addAttribute("filter", filter);
         return "main";
     }
 
@@ -40,18 +46,6 @@ public class MainController {
         Message message = new Message(text, tag, user);
         messageRepository.save(message);
         Iterable<Message> messages = messageRepository.findAll();
-        model.put("messages", messages);
-        return "main";
-    }
-
-    @PostMapping("/filter")
-    public String filter(@RequestParam String filter, Map<String, Object> model) {
-        Iterable<Message> messages;
-        if (filter != null && !filter.isEmpty()) {
-            messages = messageRepository.findByTag(filter);
-        } else {
-            messages = messageRepository.findAll();
-        }
         model.put("messages", messages);
         return "main";
     }
